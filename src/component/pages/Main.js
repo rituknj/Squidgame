cf1dc86ce108368581f6198db4c9f1b8b101b4a7
Mayank Rajput <rituknj@gmail.com>
2023-04-13 20:57:33 +0530

    image

--- src/component/pages/Main.js
+++ src/component/pages/Main.js
@@ -6,6 +6,8 @@ import { BsPlayCircleFill } from "react-icons/bs";
 import pinksale from "../Image/pinksalelogo.png";
 import deximg from "../Image/dexviewlogo.png";
 import mintimg from "../Image/nft_mint.png";
+import dosacrat from "../Image/dosacract_mint_logo.png";
+import passimg from "../Image/bomb_apply.png"
 
 export default function Main() {
   return (
@@ -62,17 +64,79 @@ export default function Main() {
                   <img src={deximg} alt="" />
                 </div>
               </div>
-              <div className="f-s-1_3 c-b  f-w-600 m-t-1_5">CA:0xbC93891CF9aBC3F8124B2eCE3727E208C4236C58</div>
+              <div className="f-s-1_3 c-b  f-w-600 m-t-1_5">
+                CA:0xbC93891CF9aBC3F8124B2eCE3727E208C4236C58
+              </div>
             </div>
           </Grid>
         </Grid>
       </div>
       <section className="mint">
-        <div className="">
-          <div className="">
+        <div className="row">
+          <div className="col-lg-6 col-md-6 col-sm-12 col-12">
             <div>
               <img src={mintimg} alt="" />
             </div>
+          </div>
+          <div className="col-lg-6 col-md-6 col-sm-12 col-12">
+            <div className="mint-box">
+              <div className="mint-box__price">
+                <div className="mint-box__content-title">
+                  <p>PRICE PER DOSACRAT</p>
+                  <h4>- BNB</h4>
+                </div>
+                <div className="mint-box__child">
+                  <p>TOTAL</p>
+                  <h4>- BNB</h4>
+                </div>
+              </div>
+              <div className="mint-box__title">
+                MINT YOUR
+                <img src={dosacrat} alt="" className="mint-box__title-img" />
+              </div>
+              <div className="mint-box__amount">
+                <span className="mint-box__amount-sp">-</span>
+                <div className="mint-box__amount-input">1</div>
+                <span className="mint-box__amount-sp">+</span>
+              </div>
+              <div className="mint-box__price-bottom">
+                <p className="mint-box__price-tit">TOTAL MINTED - </p>
+                <button className="mint-box__bu-mint">MINT</button>
+                <p className="mint-box__bo-para">FREEBIES 0</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="pass">
+        <div className="pass__pass-page">
+        <div className="pass__pass-container">
+          <div className="pass__pass-info">
+            <h1>
+              <span>BECOME A</span>
+              <br />
+              <span>DOSA BOMB</span>
+              <br />
+              <span>CONTENDER</span>
+            </h1>
+            <p>
+              Does your project deserve attention?
+              <br />
+              Does your project want a big buy pumped into the chart?
+              <br />
+              Is your community strong enough to fight for it?
+              <br />
+              <br />
+              Well here's your chance, apply to be a contender in the Dosa Bomb
+              today!
+            </p>
+            <div>
+              <button className="apply-button">APPLY NOW</button>
+            </div>
+          </div>
+          <div className="pass__pass-img">
+            <img src={passimg} alt=""/>
+          </div>
           </div>
         </div>
       </section>
